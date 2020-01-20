33a8de313b844d4451e4e1ccebd062a5c6af31b9
sadra <sadrayan@gmail.com>
2020-01-19 21:03:27 -0500

    server updates

--- public/app.js
+++ public/app.js
@@ -1,7 +1,1 @@
 var myApp = angular.module('myApp', []);
-
-var baseURL = 'http://localhost:8081/';
-myApp.value('clientId', 'a12345654321x');
-myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
-    console.log("Hello World from controller");
-}]);
